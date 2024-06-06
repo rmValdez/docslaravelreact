7f2576ea1d6a3605dece7bd44e7f49b30fc8fdf9
reign mark valdez <rmbv@newmediastaff.com>
2024-06-06 13:59:21 +0800

    Hot update

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,39 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      refresh: true,
+    }),
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  server: {
+    host: 'nmsapps.local',
+    strictPort: true,
+    port: 5190, // you can replace this port with any port
+  },
+  resolve: {
+    alias: {
+      'MaterialThemeProvider': '@mui/material/styles/ThemeProvider',
+    },
+  },
+  optimizeDeps: {
+    include: ['@mui/material/Tooltip'],
+  }
 });
-
-// export default defineConfig({
-//   plugins: [
-//     laravel({
-//       input: ['resources/css/app.css', 'resources/js/app.jsx'],
-//       refresh: true,
-//     }),
-//     react({
-//       jsxImportSource: '@emotion/react',
-//       babel: {
-//         plugins: ['@emotion/babel-plugin'],
-//       },
-//     }),
-//   ],
-//   server: {
-//     host: 'nmsapps.local',
-//     strictPort: true,
-//     port: 5190, // you can replace this port with any port
-//   },
-//   resolve: {
-//     alias: {
-//       'MaterialThemeProvider': '@mui/material/styles/ThemeProvider',
-//     },
-//   },
-//   optimizeDeps: {
-//     include: ['@mui/material/Tooltip'],
-//   }
-// });
