d9af425fd6228aca2c5b5cef285567c57d59c69b
Reign Mark <valdezreignmark@gmail.com>
2024-06-10 15:20:59 +0800

    initial commit

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+ 
+// export default defineConfig({
+//     plugins: [
+//         laravel(['resources/js/app.jsx']),
+//         react(),
+//     ],
+// });
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      refresh: true,
+    }),
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  server: {
+    host: 'docslaravelreact.local',
+    strictPort: true,
+    port: 5190, // you can replace this port with any port
+  },
+  resolve: {
+    alias: {
+      'MaterialThemeProvider': '@mui/material/styles/ThemeProvider',
+    },
+  },
+  optimizeDeps: {
+    include: ['@mui/material/Tooltip'],
+  }
 });
