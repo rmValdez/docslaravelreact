eb5fa89503fb4cf3ee29641e0158992ac5c1945a
Reign Mark <valdezreignmark@gmail.com>
2024-06-10 10:30:39 +0800

    routing

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Auth;
+use Illuminate\Support\Facades\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    return view('home');
 });
+
+
+  // Route::middleware('oauth')->group(function () {\
+  //   Route::get('/{path?}', function () {
+  //     return view('home', [ ...$data ]);
+  //   })->where('path', '^(?!oauth\/).*');
+  // });
+  
